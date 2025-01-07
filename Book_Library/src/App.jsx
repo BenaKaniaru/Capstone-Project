23f722b6ad61ce7220be9a733c90fdf4f8eee20b
Benard Kaniaru <kaniarubenard623@gmailcom>
2025-01-07 10:11:31 +0300

    create search component for handling API calls

--- Book_Library/src/App.jsx
+++ Book_Library/src/App.jsx
@@ -1,3 +1,9 @@
+import Search from "./components/Search";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 }
---
+++ Book_Library/src/components/Search.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+export default function Search() {
+  const [searchInput, setSearchInput] = useState("");
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const url = "https://openlibrary.org/search.json?title";
+
+  //function for handling form submission
+  //function ensures controlled API calls
+  async function handleSearch(e) {
+    e.preventDefault();
+    if (!searchInput.trim()) {
+      setError("Your search input is empty!");
+      return;
+    }
+
+    setError(null);
+    setLoading(true);
+
+    try {
+      const response = await fetch(`${url}=${searchInput}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data. Please try again!");
+      }
+      const data = await response.json();
+      setBooks(data.docs);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setLoading(false);
+  }
+  return (
+    <form onSubmit={handleSearch} className="block items-center">
+      {/*Search Input field */}
+      <input
+        name="searchInput"
+        value={searchInput}
+        onChange={(e) => {
+          setSearchInput(e.target.value);
+        }}
+        type="text"
+        className="border px-3 py-1 text-black rounded-l-lg focus:outline-none focus:ring-2 focus:ring-orange-500"
+      />
+      {/*form submission button */}
+      <button
+        type="submit"
+        className="border p-1 rounded-r-lg text-white w-20 bg-orange-500 hover:bg-orange-200"
+      >
+        Search
+      </button>
+
+      {/*Display error message, if any */}
+      <div className="border w-64 bg-white text-red-500 rounded-md m-4 px-4">
+        {error}
+      </div>
+    </form>
+  );
+}
