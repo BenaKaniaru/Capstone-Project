3aaa7adefe17549bbdbed2742c679d00716d195d
Benard Kaniaru <kaniarubenard623@gmailcom>
2025-01-07 10:37:20 +0300

    add Nav component for the apps navigation

--- Book_Library/src/App.jsx
+++ Book_Library/src/App.jsx
@@ -1,8 +1,10 @@
 import Search from "./components/Search";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <div>
+      <Nav />
       <Search />
     </div>
   );
---
+++ Book_Library/src/components/Nav.jsx
@@ -0,0 +1,20 @@
+export default function Nav() {
+  return (
+    <div className=" flex flex-row bg-gray-300 drop-shadow-md p-1 items-center fixed top-0 left-0 right-0">
+      <div className="w-72 py-4 ml-10">
+        <span className="text-orange-500 font-black text-2xl">NerdHub</span>
+      </div>
+      <div className="flex-1"></div>
+      <div className="w-96 flex justify-between mx-8 text-white text-lg">
+        <span className="hover:cursor-pointer hover:text-blue-500">Home</span>
+        <span className="hover:cursor-pointer hover:text-blue-500">
+          Reading List
+        </span>
+        <span className="hover:cursor-pointer hover:text-blue-500">About</span>
+        <span className="hover:cursor-pointer hover:text-blue-500">
+          Log In/Sign In
+        </span>
+      </div>
+    </div>
+  );
+}
