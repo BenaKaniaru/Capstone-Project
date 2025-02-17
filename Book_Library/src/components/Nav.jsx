d5a1a55d578602ce20e5dc57eeddc9bfb64ae444
Benard Kaniaru <kaniarubenard623@gmailcom>
2025-02-17 18:08:08 +0300

    modified the text color for the items on navigation menu and their display when active

--- Book_Library/src/components/Nav.jsx
+++ Book_Library/src/components/Nav.jsx
@@ -1,29 +1,45 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [homeOpen, setHomeOpen] = useState(false);
+  const location = useLocation();
   return (
     <header className=" flex flex-row bg-gray-500 drop-shadow-md p-1 items-center fixed top-0 left-0 right-0">
       <div className="w-72 py-4 ml-4 md:ml-10">
         <span className="text-orange-500 font-black text-2xl">NerdHub</span>
       </div>
       <div className="flex-1"></div>
-      <nav className="w-96 md:flex justify-between text-white text-lg hidden mx-10">
-        <Link to="/" className="hover:cursor-pointer hover:text-blue-500">
+      <nav className="w-80 md:flex justify-between text-orange-500 text-lg font-semibold hidden mx-10">
+        <Link
+          to="/"
+          className={
+            location.pathname === "/"
+              ? "hover:cursor-pointer underline text-orange-300"
+              : "hover:cursor-pointer hover:underline"
+          }
+        >
           Home
         </Link>
 
         <Link
           to="/ReadingList"
-          className="hover:cursor-pointer hover:text-blue-500"
+          className={
+            location.pathname === "/ReadingList"
+              ? "hover:cursor-pointer underline text-orange-300"
+              : "hover:cursor-pointer hover:underline"
+          }
         >
           Reading List
         </Link>
-        <span className="hover:cursor-pointer hover:text-blue-500">About</span>
         <Link
           to="/UserAuth"
-          className="hover:cursor-pointer hover:text-blue-500"
+          className={
+            location.pathname === "/UserAuth"
+              ? "hover:cursor-pointer underline text-orange-300"
+              : "hover:cursor-pointer hover:underline"
+          }
         >
           Log In/Sign In{" "}
         </Link>
@@ -53,24 +69,30 @@ export default function Nav() {
       </button>
       {/*mobile navigation for small screen sizes*/}
       {menuOpen && (
-        <nav className="absolute top-16 right-0 bg-gray-700 text-white flex flex-col items-left w-40 pl-4 py-4 gap-4 md:hidden">
-          <Link to="/" className="hover:cursor-pointer hover:text-blue-500">
-            Home
+        <nav className="absolute top-16 right-0 bg-gray-500 text-orange-500 flex flex-col items-left w-40 pl-4 py-4 gap-4 font-semibold md:hidden">
+          <Link to="/" className="hover:cursor-pointer hover:underline">
+            <span
+              onClick={() => {
+                setHomeOpen(true);
+              }}
+            >
+              {homeOpen && (
+                <>
+                  <span className="underline">Home</span>
+                </>
+              )}
+
+              {!homeOpen && <span>Home</span>}
+            </span>
           </Link>
 
           <Link
             to="/ReadingList"
-            className="hover:cursor-pointer hover:text-blue-500"
+            className="hover:cursor-pointer hover:underline"
           >
             Reading List
           </Link>
-          <span className="hover:cursor-pointer hover:text-blue-500">
-            About
-          </span>
-          <Link
-            to="/UserAuth"
-            className="hover:cursor-pointer hover:text-blue-500"
-          >
+          <Link to="/UserAuth" className="hover:cursor-pointer hover:underline">
             Log In/Sign In{" "}
           </Link>
         </nav>
